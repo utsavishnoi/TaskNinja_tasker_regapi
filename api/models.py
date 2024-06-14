1e266dae1e66606fd2969574d61b702396bdde04
rex <rohitrex321@gmail.com>
2024-06-14 17:05:35 +0530

    Multiple Address APIs

--- api/models.py
+++ api/models.py
@@ -1,5 +1,3 @@
-# models.py
-
 from django.db import models
 
 class Tasker(models.Model):
@@ -8,9 +6,22 @@ class Tasker(models.Model):
     email = models.EmailField(unique=True)
     password = models.CharField(max_length=100)
     service = models.CharField(max_length=100)
-    state = models.CharField(max_length=100)
-    city = models.CharField(max_length=100)
     about = models.CharField(max_length=100)
+    is_active = models.BooleanField(default=True)
+    price_per_hour = models.CharField(max_length=5, blank=True)
+
+    def __str__(self):
+        return self.name
+
+class Address(models.Model):
+    tasker = models.ForeignKey(Tasker, related_name='addresses', db_column='tasker_id_id', on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    state = models.CharField(max_length=100, blank=True)
+    city = models.CharField(max_length=100)
+    full_address = models.CharField(max_length=500)
+
+    class Meta:
+        unique_together = ('tasker', 'name')  # Ensures name is unique per tasker
 
     def __str__(self):
         return self.name
--- api/views.py
+++ api/views.py
@@ -1,23 +1,25 @@
 from rest_framework import status
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from .models import Tasker
-from .serializers import TaskerSerializer
-from django.contrib.auth.hashers import make_password
-
 from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
-from django.db import connection
+from django.contrib.auth.hashers import make_password
+from .serializers import TaskerSerializer
 
 @api_view(['POST'])
 @permission_classes([AllowAny])
 def register_tasker(request):
     if request.method == 'POST':
         data = request.data.copy()
+        
+        # Hash the password before saving
         data['password'] = make_password(data['password'])
-        serializer = TaskerSerializer(data=data)
-        if serializer.is_valid():
-            serializer.save()
-            print(connection.queries)
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+        # Serialize the Tasker data
+        tasker_serializer = TaskerSerializer(data=data)
+        
+        # Validate and save Tasker data
+        if tasker_serializer.is_valid():
+            tasker_serializer.save()
+            return Response(tasker_serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(tasker_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
