2728c085e7d2cc33b05b586ef3be5bfd06cc4a24
rex <rohitrex321@gmail.com>
2024-07-22 17:21:43 +0530

    api module removed

--- api/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Tasker
-
-class TaskerSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Tasker
-        fields = ['tasker_id','username','email','password','about','is_active','service','price','address_state','address_city','address_full','pincode','phone_no']
--- api/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import register_tasker,list_taskers_by_service
-
-urlpatterns = [
-    path('taskers/register/', register_tasker, name='register_tasker'),
-    path('api/taskers/<str:service_name>/', list_taskers_by_service, name='list_taskers_by_service'),
-]
